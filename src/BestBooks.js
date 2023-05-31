c04e62578a96bc5b11783daab3f6baeed67e4a31
CEEJ987 <123979158+CEEJ987@users.noreply.github.com>
2023-05-31 15:54:32 -0500

    update assignment

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,24 +1,38 @@
 import React, { useState } from 'react';
 
-function BestBooks (){
-const [books, setBooks] = useState([])
-  /* TODO: Make a GET request to your API to fetch all the books from the database  */
 
+function BestBooks (){
+  const [books, setBooks] = useState([])
+  /* TODO: Make a GET request to your API to fetch all the books from the database  */
   
-    /* TODO: render all the books in a Carousel */
+  async function getBestBooks() {
+      let Getbooks = await axios.get(`http://localhost:3001/books?=${books}`)
+      setBooks(books.data)
+      console.log(books.data)
+    }
+    getBestBooks();
+  
+  /* TODO: render all the books in a Carousel */
 
     return (
-      <>
-        <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
+        <>
+      <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
 
-        {books.length ? (
-          <p>Book Carousel coming soon</p>
-        ) : (
-          <h3>No Books Found :(</h3>
-        )}
-      </>
-    )
-  }
-
+      {books.length ? (
+        <Carousel>
+          {books.map(book => (
+            <div key={book._id}>
+              <h3>{book.title}</h3>
+              <p>{book.description}</p>
+              <p>Status: {book.status}</p>
+            </div>
+          ))}
+        </Carousel>
+      ) : (
+        <h3>No Books Found :</h3>
+      )}
+    </>
+  );
+}
 
 export default BestBooks;
